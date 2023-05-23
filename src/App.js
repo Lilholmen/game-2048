1a0ef056a51f026072e7c9dcd19c6ffa0629c652
Artur Yandulsky <artur.yandulsky@jetruby.com>
2023-05-23 17:40:30 +0300

    backup commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Control from "./components/Control/Control";
+import GameBoard from "./components/GameBoard";
+import { TilesContextProvider } from "./context/tilesContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-stone-700">
+      <TilesContextProvider>
+        <GameBoard />
+        <Control />
+      </TilesContextProvider>
     </div>
   );
 }
